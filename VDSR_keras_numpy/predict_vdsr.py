[UPDATE] removed normalization, added min pixel clip to 0, fixed multiple result files stitching

VDSR_keras_numpy/predict_vdsr.py
@@ -3,6 +3,9 @@ import os
 from keras.models import model_from_json
 import numpy as np
 
+# import train_vdsr
+
+
 import yaml
 from yaml import Loader
 
@@ -18,94 +21,117 @@ def num_char(x):
 
 def stitch_numpy_slices():
     np_slices_path = config_params['np_slices_path']
+    output_path = config_params['np_stitched_output_path']
 
-    output_path = './numpy_stitch_output/'
-
+    """ Get all the files under the results directory"""
     np_slices = os.listdir(np_slices_path)
 
-    # remove non-numpy things from the list
-    for one_file in np_slices:
-        if '.npy' not in one_file:
-            np_slices.remove(one_file)
+    """
+         remove non-numpy things from the list
+         segregate slices for the same chunk 
+    """
 
-    np_slices.sort(key=num_char)
-    print(np_slices)
-    # stitch same files
-
-    file_basename = ''
-    output = None
+    seg_files = {}
     for one_file in np_slices:
-        print(one_file)
-        if file_basename == '':
+
+        if '.npy' in one_file:
+
+            """ Get the file basename"""
             file_basename = one_file.split('_')[0]
-            print(file_basename)
-            # get the numpy data from the file
-            output = np.load(np_slices_path+one_file)
-            output = np.reshape(output, (output.shape[0], output.shape[1], 1))
+            file_to_add = np_slices_path + one_file
 
-        else:
-            if file_basename in one_file:
-                tmp = np.load(np_slices_path + one_file)
-                tmp = np.reshape(tmp, (tmp.shape[0], tmp.shape[1], 1))
-                output = np.append(output, tmp, axis=2)
+            if file_basename not in seg_files:
+
+                """ If not seg file dict """
+                tmp = list()
+                tmp.append(file_to_add)
+                seg_files[file_basename] = tmp
+
             else:
 
-                # first save the previous file and reset the file_basename and output
-                print("Saving the stitched file : " + str(output.shape))
-                np.save(file=output_path+file_basename+".npy", arr=output)
-                output = np.empty()
+                """ if already in seg_files list"""
+                tmp = seg_files[file_basename]
+                tmp.append(file_to_add)
+                seg_files[file_basename] = tmp
 
-                file_basename = one_file.split('_')[0]
 
-                # get the numpy data from the file
-                tmp = np.load(np_slices_path + one_file)
+    for one_base_file in seg_files.keys():
+        print(" working on :" + one_base_file)
+        base_name = one_base_file
+        all_slices = seg_files[one_base_file]
+
+        print("Total slices :" , len(all_slices))
+        """ sort all the slices in place """
+        all_slices.sort(key=num_char)
+
+        output = None
+        for one_slice in all_slices:
+            if output is None:
+                output = np.load(one_slice)
+                output = np.reshape(output, (output.shape[0], output.shape[1], 1))
+            else:
+                tmp = np.load(one_slice)
                 tmp = np.reshape(tmp, (tmp.shape[0], tmp.shape[1], 1))
                 output = np.append(output, tmp, axis=2)
 
-    if(file_basename != ''):
-        print("Saving the stitched file : " + str(output.shape))
-        np.save(file=output_path + file_basename + ".npy", arr=output)
+        print(" Saving the stitched file with shape: " + str(output.shape))
+        np.save(file= output_path + str(base_name) + ".npy", arr=output)
 
+    return
 
 
 
 if __name__ == '__main__':
 
+    run_test = True
+
+    if run_test:
+
+        json_path = config_params['json_path']
+        w_path = sys.argv[1]
+        img_path = config_params['test_images_path']
+        dst_path = config_params['test_dest_path']
+        target_size = config_params['pred_tgt_size']
+
+        print("--------------------------------")
+        print('json_path : ', json_path)
+        print('w_path : ', w_path)
+        print('img_path : ', img_path)
+        print('dst_path : ', dst_path)
+        print("--------------------------------")
+
+        with open(json_path, 'r') as f:
+            vdsr = model_from_json(f.read())
+        vdsr.load_weights("checkpoints/" + w_path)
+
+        li = os.listdir(img_path)
+
+        target_path = '%s/%s/' % (img_path, dst_path)
+        os.makedirs(target_path, exist_ok=True)
+
+        for filename in li:
+            if filename[-4:] == '.npy':
+                print("executing :", filename)
+                np_file = os.path.join(img_path, filename)
+
+                """  Load numpy file  and reshape """
+                inp_pixels = np.load(np_file)
+                inp_pixels = np.reshape(inp_pixels, (1, 1, inp_pixels.shape[0], inp_pixels.shape[1]))
+
+                max_scale = inp_pixels.max()
+                """ Get the output from the model and reshape"""
+                out_pixels = vdsr.predict(inp_pixels)
+
+                out_pixels = np.reshape(out_pixels, target_size)
+
+                # normalize the pixels based on the max pixel value of the input image
+                # out_pixels = max_scale*(out_pixels - np.min(out_pixels)) / np.ptp(out_pixels)
+                out_pixels = np.clip(a=out_pixels,a_min=0, a_max=out_pixels.max())
+                np.save(file=target_path + "/" + filename, arr=out_pixels)
+
+    else:
+        print(" ++ Not Running the test segment. ++ ")
+
     """ generates the np_stitched file """
+
     stitch_numpy_slices()
-    exit()
-
-    json_path = config_params['json_path']
-    w_path = sys.argv[1]
-    img_path = config_params['test_images_path']
-    dst_path = config_params['test_dest_path']
-    target_size = config_params['pred_tgt_size']
-
-    print("--------------------------------")
-    print('json_path : ', json_path)
-    print('w_path : ', w_path)
-    print('img_path : ', img_path)
-    print('dst_path : ', dst_path)
-    print("--------------------------------")
-
-    with open(json_path, 'r') as f:
-        vdsr = model_from_json(f.read())
-    vdsr.load_weights("checkpoints/" + w_path)
-
-    li = os.listdir(img_path)
-
-    target_path = '%s/%s/' % (img_path, dst_path)
-    os.makedirs(target_path, exist_ok=True)
-
-    for filename in li:
-        if filename[-4:] == '.npy':
-            print("executing :", filename)
-            np_file = os.path.join(img_path, filename)
-            inp_pixels = np.load(np_file)
-            inp_pixels = np.reshape(inp_pixels, (1, 1, inp_pixels.shape[0], inp_pixels.shape[1]))
-
-            out_pixels = vdsr.predict(inp_pixels)
-
-            out_pixels = np.reshape(out_pixels, target_size)
-
-            np.save(file=target_path + "/" + filename, arr=out_pixels)

VDSR_keras_numpy/train_vdsr.py
@@ -47,8 +47,9 @@ def normalize_data(raw_data):
 
 def generate_numpy_slices():
 
+	"""	get all the numpy data and generate the slices	"""
+
 	np_files = {}
-	# get all the numpy data and generate the slices
 	np_files['data_x_path'] = os.listdir(config_params['data_x_path'])
 	np_files['data_y_path'] = os.listdir(config_params['data_y_path'])
 	np_files['data_test_path'] = os.listdir(config_params['data_test_path'])
@@ -167,8 +168,8 @@ def get_data_batch(target_list, offset):
 
 		x, y = load_numpy_data(x_data, y_data, patch_size=patch_size)
 
-		x = normalize_data(x)
-		y = normalize_data(y)
+		# x = normalize_data(x)
+		# y = normalize_data(y)
 
 
 		# x = np.reshape(x, (x.shape[0], x.shape[1], 1))
@@ -222,85 +223,58 @@ def SSIM(y_true, y_pred):
 	return tf.image.ssim(y_pred, y_true, config_params['max_pixel_val'])
 
 
-""" generate the numpy 2D slices form the numpy volumes """
-generate_numpy_slices()
 
-"""	Get the training and testing data """
+if __name__ == '__main__':
 
-img_list = get_data_list(DATA_X_PATH)
-imgs_to_train = (len(img_list) * TRAIN_TEST_RATIO[0] )// 10
+	""" generate the numpy 2D slices form the numpy volumes """
+	generate_numpy_slices()
 
-train_list = img_list[:imgs_to_train]
-val_list = img_list[imgs_to_train:]
+	"""	Get the training and testing data """
 
+	img_list = get_data_list(DATA_X_PATH)
+	imgs_to_train = (len(img_list) * TRAIN_TEST_RATIO[0]) // 10
 
+	train_list = img_list[:imgs_to_train]
+	val_list = img_list[imgs_to_train:]
 
-input_img = Input(shape=(1, None, None))
+	input_img = Input(shape=(1, None, None))
 
-model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(input_img)
-model = Activation('relu')(model)
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model1 = Activation('relu')(model)
-#
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model1)
-# model = Activation('relu')(model)
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model2 = Activation('relu')(model1)
-#
-# model = add([model2, model1])
-#
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model = Activation('relu')(model)
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model3 = Activation('relu')(model)
-#
-# model = add([model3, model2])
-#
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model = Activation('relu')(model)
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model4 = Activation('relu')(model)
-# model = add([model4, model3])
+	model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(input_img)
+	model = Activation('relu')(model)
 
+	model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
+	model = Activation('relu')(model)
 
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model = Activation('relu')(model)
-# model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-# model = Activation('relu')(model)
-model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-model = Activation('relu')(model)
-model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-model = Activation('relu')(model)
-model = Conv2D(1, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
-res_img = model
+	model = Conv2D(64, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
+	model = Activation('relu')(model)
 
-output_img = add([res_img, input_img])
-#output_img = res_img
-model = Model(input_img, output_img)
+	model = Conv2D(1, (3, 3), padding='same', kernel_initializer='he_normal', data_format='channels_first')(model)
 
+	res_img = model
 
-adam = Adam(lr=LR)
-# sgd = SGD(lr=1e-5, momentum=0.9, decay=1e-5, nesterov=False)
-model.compile(adam, loss='mse', metrics=[PSNR, "accuracy"])
-model.summary()
+	output_img = add([res_img, input_img])
+	# output_img = res_img
+	model = Model(input_img, output_img)
 
-with open('./model/vdsr_architecture.json', 'w') as f:
-	f.write(model.to_json())
+	adam = Adam(lr=LR)
+	# sgd = SGD(lr=1e-5, momentum=0.9, decay=1e-5, nesterov=False)
+	model.compile(adam, loss='mse', metrics=[PSNR, "accuracy"])
+	model.summary()
 
+	with open('./model/vdsr_architecture.json', 'w') as f:
+		f.write(model.to_json())
 
-filepath="./checkpoints/model-{epoch:02d}-{PSNR:.2f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor=PSNR, verbose=1, mode='max',period=config_params['save_every'])
+	filepath = "./checkpoints/model-{epoch:02d}-{PSNR:.2f}.hdf5"
+	checkpoint = ModelCheckpoint(filepath, monitor=PSNR, verbose=1, mode='max', period=config_params['save_every'])
 
-lr_scheduler = ReduceLROnPlateau(monitor='val_PSNR', factor=0.5, patience=10, min_lr=0.00001)
+	lr_scheduler = ReduceLROnPlateau(monitor='val_PSNR', factor=0.5, patience=10, min_lr=0.00001)
 
-callbacks_list = [checkpoint,lr_scheduler]
+	callbacks_list = [checkpoint, lr_scheduler]
 
+	model.fit_generator(data_gen(train_list), steps_per_epoch=config_params['steps_per_epoch'], \
+						validation_data=data_gen(val_list), validation_steps=len(val_list) // BATCH_SIZE, \
+						epochs=EPOCHS, use_multiprocessing=True, workers=1, callbacks=callbacks_list)
 
-model.fit_generator(data_gen(train_list), steps_per_epoch=config_params['steps_per_epoch'], \
-					validation_data=data_gen(val_list), validation_steps=len(val_list) // BATCH_SIZE, \
-					epochs=EPOCHS, use_multiprocessing=True, workers=1, callbacks=callbacks_list)
+	model.save('./model/vdsr_model.h5')  # creates a HDF5 file
 
-
-model.save('./model/vdsr_model.h5')  # creates a HDF5 file
-
-del model  # deletes the existing model
+	del model  # deletes the existing model